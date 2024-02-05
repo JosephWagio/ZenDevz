02894b5fbb40f7948e955a79f90333940a551825
Major Pinky <107479804+JosephWagio@users.noreply.github.com>
2024-02-05 17:47:44 +0100

    test

--- src/Containers/Header/header.jsx
+++ src/Containers/Header/header.jsx
@@ -18,32 +18,34 @@ const Header = () => {
   };
   
   return (
-    <div className='header section__padding' id='home'>
-      <div className='header-content'>
-        <h3 className='p__opensans'>DO IT BETTER TODAY</h3>
-        <h1 className="headtext__cormorant">
-          Let's {' '}
-          <span className="headtext__cormorant">
-            <Typewriter
-              words={['Make Your Website', 'Manage Your Social Media']}
-              loop={100}
-              cursor 
-              cursorColor='#FFCC29'
-              cursorStyle='|'
-              typeSpeed={120}
-              deleteSpeed={100}
-              delaySpeed={1000}
-            />
-          </span>
-        </h1>
-        <p className='p__cormorant' style={{margin: '2rem 0'}}>We offer Comprehensive web solutions: <br/> development, SEO mastery, and digital excellence, tailored for success.</p>
-        <div className="header-content__contact">
-         <button className='button'><Link to="/contact-us">Get Started</Link></button>
+    <div className='header' id='home'>
+      <div className='section__margin'>
+        <div className='header-content'>
+          <h3 className='p__opensans'>DO IT BETTER TODAY</h3>
+          <h1 className="headtext__cormorant">
+            Let's {' '}
+            <span className="headtext__cormorant">
+              <Typewriter
+                words={['Make Your Website', 'Manage Your Social Media']}
+                loop={100}
+                cursor 
+                cursorColor='#FFCC29'
+                cursorStyle='|'
+                typeSpeed={120}
+                deleteSpeed={100}
+                delaySpeed={1000}
+              />
+            </span>
+          </h1>
+          <p className='p__cormorant' style={{margin: '2rem 0'}}>We offer Comprehensive web solutions: <br/> development, SEO mastery, and digital excellence, tailored for success.</p>
+          <div className="header-content__contact">
+          <button className='button'><Link to="/contact-us">Get Started</Link></button>
+          </div>
         </div>
+        <motion.div className="header-image" transition={bounceTransition} animate={{ y: [10, -10, 10]}} >
+          <img src={HeaderImg} alt="img" />
+        </motion.div>
       </div>
-      <motion.div className="header-image" transition={bounceTransition} animate={{ y: [10, -10, 10]}} >
-        <img src={HeaderImg} alt="img" />
-      </motion.div>
     </div>
   )
 }
